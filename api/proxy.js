c6c73552086b1b53b642349ab1adb247d8b8ba9f
G0dvv <94789068+G0dvv@users.noreply.github.com>
2023-12-20 15:58:21 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://now.gg/play/roblox-corporation/5349/roblox"; // Your website URL
+  let target = "https://108.181.34.45/play/roblox-corporation/5349/roblox?__cpo=aHR0cHM6Ly9ub3cuZ2c"; // Your website URL
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
     changeOrigin: true,
     pathRewrite: {
       // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
+      //  /backend/user/login => https://108.181.34.45/play/roblox-corporation/5349/roblox?__cpo=aHR0cHM6Ly9ub3cuZ2c
       //   "^/backend/": "/",d
     },
   })(req, res);
