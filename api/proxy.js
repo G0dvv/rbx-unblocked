46bb1184f1d954849c1acb0d9ce247eeef8b8b1a
G0dvv <94789068+G0dvv@users.noreply.github.com>
2023-12-20 16:52:33 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://basketcraft.net/uv/service/hvtrs8%2F-nmw%2Cge%2Frlcy-rmbnoz-aoppmrctkol%2F7369-rmbnoz"; // Your website URL
+  let target = "https://ubg100.vercel.app"; // Your website URL
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
     changeOrigin: true,
     pathRewrite: {
       // rewrite request path `/backend`
-      //  /backend/user/login => https://108.181.34.45/play/roblox-corporation/5349/roblox?__cpo=aHR0cHM6Ly9ub3cuZ2c
+      //  /backend/user/login => 
       //   "^/backend/": "/",d
     },
   })(req, res);
