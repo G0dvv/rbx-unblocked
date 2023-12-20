e6fc40583cd7f9b8f8427ed4e520c5205fb893d6
G0dvv <94789068+G0dvv@users.noreply.github.com>
2023-12-20 16:33:23 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://108.181.34.45/play/roblox-corporation/5349/roblox?__cpo=aHR0cHM6Ly9ub3cuZ2c"; // Your website URL
+  let target = "https://108.181.32.61/__cpi.php?s=UkQ2YXlSaWJuc3ZoeGR2dG04WW9LcTAzRnJmN2ladlZmem1WbUkydXhzWTlNRFFRRmovbW00RW1QYWRjb1JkMlBlTy9KNDFkSi9GTU1YR2s3VTEvNjRNb1VGSys3Y3BZbVdRcC8xM2luYVE9&r=aHR0cHM6Ly8xMDguMTgxLjMyLjYxLz9fX2Nwbz1hSFIwY0hNNkx5OXViM2N1WjJj&__cpo=1"; // Your website URL
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
